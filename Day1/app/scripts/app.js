9d01b1895beb50d416e2ca96e462db4eb3609222
Derushio <narumi18wa@gmail.com>
2015-10-04 15:31:40 +0900

    Day1プロジェクトを追加

---
+++ Day1/app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name day1App
+ * @description
+ * # day1App
+ *
+ * Main module of the application.
+ */
+angular
+  .module('day1App', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ Day1/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name day1App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the day1App
+ */
+angular.module('day1App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
